e427e4451f2a6204b2f9af4904fd2c1a283cd8d4
Barry Wu <barryxwu@gmail.com>
2020-07-02 21:53:00 +1000

    Initial socket.io commit
    Explored event on/emit with clicker

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 const express = require('express')
 const path = require('path')
+const http = require('http')
+const socketio = require('socket.io')
 
 // Setup App
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+// Config
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname,'../public')
 
+
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+let count = 0
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection!')
+
+    socket.emit('countUpdated', count)
+    socket.on('increment', () => {
+        count++
+        socket.emit('countUpdated', count)
+    })
+})
+
+server.listen(port, () => {
     console.log('Listening on port ' + port)
 })

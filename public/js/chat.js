95403f09f78821440bd2e6c116186e72b5525004
Barry Wu <barryxwu@gmail.com>
2020-07-05 12:09:54 +1000

    Disable buttons when processing, refocus to input

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,20 +4,35 @@ socket.on('message', (msg) => {
     console.log(msg)
 })
 
-document.querySelector('.form-data').addEventListener('submit', (e) => {
-    e.preventDefault()
+// Elements
+const $messageForm = document.querySelector('.form-data')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#location-data')
 
+$messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    $messageFormInput.setAttribute('disabled','disabled')
+    $messageFormButton.setAttribute('disabled','disabled')
     const msg = e.target.elements.message.value
 
     socket.emit('message', msg, () => {
+        $messageFormInput.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+        $messageFormButton.removeAttribute('disabled')
+
         console.log('Message delivered!')
     })
 })
 
-document.querySelector('#location-data').addEventListener('click', () => {
+$sendLocationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported in your browser.')
     } 
+    
+    $sendLocationButton.setAttribute('disabled','disabled')
 
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
@@ -25,6 +40,7 @@ document.querySelector('#location-data').addEventListener('click', () => {
             longitude: position.coords.longitude
         }, () => {
             console.log('Location shared!')
+            $sendLocationButton.removeAttribute('disabled')
         })
     })
 })
